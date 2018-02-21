8ad50a0b68eed72f2c3efd1ba8a1ec45bde5bbd7
Gabriel Brandao <gabrielbrandaodev@gmail.com>
2018-02-20 22:22:49 -0300

    Primeiro Commit

---
+++ FrontEnd/Controllers/ContaController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FrontEnd.Controllers
+{
+    public class ContaController : Controller
+    {
+        public IActionResult Listar()
+        {
+            return View();
+        }
+
+        public ActionResult Incluir()
+        {
+            return View();
+        }
+    }
+}
