7ded443c7c94f6948ff195a0a519ca4f2e52c4f3
Gabriel Brandao <gabrielbrandaodev@gmail.com>
2018-03-14 22:02:37 -0300

    Novas telas

--- FrontEnd/Controllers/ContaController.cs
+++ FrontEnd/Controllers/ContaController.cs
@@ -2,20 +2,37 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using FrontEnd.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace FrontEnd.Controllers
 {
     public class ContaController : Controller
     {
+        public static IList<Conta> lista = new List<Conta>();
+
         public IActionResult Listar()
+        {
+            return View(lista);
+        }
+
+        [HttpGet]
+        public ActionResult Incluir()
         {
             return View();
         }
 
-        public ActionResult Incluir()
+        [HttpPost]
+        public ActionResult Incluir(Conta conta)
         {
-            return View();
+            lista.Add(conta);
+            return RedirectToAction("Listar");
+        }
+        
+        public ActionResult Excluir(int id)
+        {
+            Conta contaRemover = lista.FirstOrDefault(x => x.Id == id);
+            return View(contaRemover);
         }
     }
 }
---
+++ FrontEnd/Models/Conta.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FrontEnd.Models
+{
+    public class Conta
+    {
+        public int Id { get; set; }
+
+        public string Descricao { get; set; }
+    }
+}
