466ca1470899052d43a4a476bed155112caed91c
Gabriel Brandao <gabrielbrandaodev@gmail.com>
2018-04-03 22:16:27 -0300

    Ajustes

---
+++ Apresentacao/Controllers/ContaController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Apresentacao.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Apresentacao.Controllers
+{
+    public class ContaController : Controller
+    {
+        public static IList<Conta> lista = new List<Conta>();
+
+        public IActionResult Listar()
+        {
+            return View(lista);
+        }
+
+        [HttpGet]
+        public ActionResult Incluir()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Incluir(Conta conta)
+        {
+            lista.Add(conta);
+            return RedirectToAction("Listar");
+        }
+
+        public ActionResult Excluir(int id)
+        {
+            Conta contaRemover = lista.FirstOrDefault(x => x.Id == id);
+            return View(contaRemover);
+        }
+    }
+}
